29bce2a55e138a72b8e89e75b0a659d64ce93208
Halil AKAY <halil0akay@gmail.com>
2017-09-25 21:25:38 +0300

    Son İşlemler

---
+++ decoratorFonksiyon/app.py
@@ -0,0 +1,21 @@
+import time
+def zamanHesapla(func):
+    def uygula(sayilar):
+        baslama=time.time()
+        sonuc=func(sayilar)
+        bitis=time.time()
+
+        print(func.__name__+" fonksiyonunun çalışması "+str(bitis-baslama)+" saniye sürdü")
+        return sonuc
+    return uygula
+
+@zamanHesapla
+def kareHesapla(sayilar):
+    sonuc=list()
+    for i in sayilar:
+        sonuc.append(i**2)
+    return sonuc
+
+kareHesapla(range(100000))
+
+#raise "Hata Fırlatıldı mı?"
