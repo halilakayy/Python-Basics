ac92b296aedfc2a9b94e03e8b52964777f95dda1
Halil AKAY <halil0akay@gmail.com>
2017-09-27 13:07:11 +0300

    son değişiklikler

--- dateTime/app.py
+++ dateTime/app.py
@@ -1,6 +1,22 @@
 from datetime import datetime
 import locale
 
-locale.setlocale(locale.LC_ALL, "")
+locale.setlocale(locale.LC_ALL, "Turkish_Turkey.1254")
 print(datetime.now().strftime("%D"))
 print(datetime.now().strftime("%d %B %Y"))
+print(locale.setlocale(locale.LC_ALL, ""))
+
+tarih=datetime.now()
+
+saniye=datetime.timestamp(tarih)
+normal=datetime.fromtimestamp(saniye)
+
+print("""
+*********************************************
+Zaman İşlemleri
+*********************************************
+""")
+
+sonrakiGun=saniye+(24*60*60)
+oncekiGun=saniye-(24*60*60)
+print("Önceki Gün: {} \nSonraki Gün: {}".format(datetime.fromtimestamp(oncekiGun).strftime("%d %B %Y"), datetime.fromtimestamp(sonrakiGun).strftime("%d %B %Y")))
